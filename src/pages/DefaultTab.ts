dfe39534196c0da753d2fc61a5b656470c5e4e0b
noddycha <nadiger.chaitanya@gmail.com>
2019-08-02 12:03:38 +0530

    Adding tab values

--- src/pages/DefaultTab.ts
+++ src/pages/DefaultTab.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
-let fs = require("fs");
-let path = require("path");
+// let fs = require("fs");
+// let path = require("path");
 import * as config from "config";
 
 export class DefaultTab {
@@ -19,34 +19,7 @@ export class DefaultTab {
                     </head>
 
                     <body>
-                        <p>
-                            These are links to the source code for all of the example dialogs given in the template.
-                        </p>`;
-
-                let files = fs.readdirSync("./public/exampleDialogs");
-                for (let i = 0; i < files.length; i++) {
-                    let currFile = files[i];
-                    let fileName = path.parse(currFile).name;
-                    let fileExtension = path.parse(currFile).ext;
-                    if (fileExtension === ".txt") {
-                        htmlPage += `<br>
-                        <a href="/exampleDialogs/` + fileName +  `.txt">` + fileName + `.ts</a>`;
-                    }
-                }
-
-                htmlPage += `
-                    <br>
-                    <br>
-                    <br>
-                    <br>
                     <p id="currentTheme">Current theme will show here when you change it in Teams settings - it can be found on the initial load by fetching the context</p>
-                    <br>
-                    <button onclick="showAllCommands()">Click to See All Commands</button>
-                    <br>
-                    <p>NOTE: Trying to get the deeplink when this is a static tab does not work. This feature only works when this is a configurable tab.</p>
-                    <button onclick="getDeeplink()">Click to get a deeplink to this tab</button>
-                    <br>
-                    <br>
                     <button onclick="showContext()">Click to Show Tab's Context</button>
                     <p id="contextOutput"></p>
                     <script>
