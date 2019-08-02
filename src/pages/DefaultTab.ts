4f5a46cc642659a54150f036b94ac31bea216cbc
noddycha <nadiger.chaitanya@gmail.com>
2019-08-02 11:58:58 +0530

    Adding comtext back

--- src/pages/DefaultTab.ts
+++ src/pages/DefaultTab.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
-// let fs = require("fs");
-// let path = require("path");
+let fs = require("fs");
+let path = require("path");
 import * as config from "config";
 
 export class DefaultTab {
@@ -19,29 +19,57 @@ export class DefaultTab {
                     </head>
 
                     <body>
-                        <h2> Welcome, </h2> <h3 id="username"> </h3>
-                        <button value="Get Username" onclick="getUsername()"/>
+                        <p>
+                            These are links to the source code for all of the example dialogs given in the template.
+                        </p>`;
+
+                let files = fs.readdirSync("./public/exampleDialogs");
+                for (let i = 0; i < files.length; i++) {
+                    let currFile = files[i];
+                    let fileName = path.parse(currFile).name;
+                    let fileExtension = path.parse(currFile).ext;
+                    if (fileExtension === ".txt") {
+                        htmlPage += `<br>
+                        <a href="/exampleDialogs/` + fileName +  `.txt">` + fileName + `.ts</a>`;
+                    }
+                }
+
+                htmlPage += `
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <p id="currentTheme">Current theme will show here when you change it in Teams settings - it can be found on the initial load by fetching the context</p>
+                    <br>
+                    <button onclick="showAllCommands()">Click to See All Commands</button>
+                    <br>
+                    <p>NOTE: Trying to get the deeplink when this is a static tab does not work. This feature only works when this is a configurable tab.</p>
+                    <button onclick="getDeeplink()">Click to get a deeplink to this tab</button>
+                    <br>
+                    <br>
+                    <button onclick="showContext()">Click to Show Tab's Context</button>
+                    <p id="contextOutput"></p>
                     <script>
                         var microsoftTeams;
 
                         $(document).ready(function () {
                             microsoftTeams.initialize();
                             microsoftTeams.registerOnThemeChangeHandler(function(theme) {
-                                // document.getElementById('currentTheme').innerHTML = theme;
+                                document.getElementById('currentTheme').innerHTML = theme;
                             });
                         });
 
-                        // function showAllCommands() {
-                        //     window.location = "${config.get("app.baseUri") + "/allCommands"}";
-                        // }
+                        function showAllCommands() {
+                            window.location = "${config.get("app.baseUri") + "/allCommands"}";
+                        }
 
-                        // function getDeeplink() {
-                        //     microsoftTeams.shareDeepLink({subEntityId: 'stuff', subEntityLabel: 'stuff2'});
-                        // }
+                        function getDeeplink() {
+                            microsoftTeams.shareDeepLink({subEntityId: 'stuff', subEntityLabel: 'stuff2'});
+                        }
 
-                        function getUsername() {
+                        function showContext() {
                             microsoftTeams.getContext((context) => {
-                                document.getElementById('username').innerHTML = JSON.stringify(context).loginHint;
+                                document.getElementById('contextOutput').innerHTML = JSON.stringify(context);
                             });
                         }
                     </script>
