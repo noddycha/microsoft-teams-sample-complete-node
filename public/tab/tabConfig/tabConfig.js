b93342d2f68d9f0a8aa95b0d9ca98b3a66484070
noddycha <nadiger.chaitanya@gmail.com>
2019-08-11 13:23:21 +0530

    Adding silent auth code

--- public/tab/tabConfig/tabConfig.js
+++ public/tab/tabConfig/tabConfig.js
@@ -8,15 +8,23 @@ $(document).ready(function () {
   microsoftTeams.settings.registerOnSaveHandler(function (saveEvent) {
     if(document.getElementById('auth-selector').value == "user-info") {
       microsoftTeams.settings.setSettings({
-        suggestedDisplayName: "Axyn Silent Auth (User Info)",
+        suggestedDisplayName: "Axyn User Info",
         contentUrl: createTabUrl("user-info"),
         entityId: "AxynUbboUser",
         websiteUrl: createTabUrl("user-info"),
       });
       saveEvent.notifySuccess();
+    } else if(document.getElementById('auth-selector').value == "silent-auth") {
+      microsoftTeams.settings.setSettings({
+        suggestedDisplayName: "Axyn Silent Auth",
+        contentUrl: createTabUrl("silent-auth"),
+        entityId: "AxynUbboUser",
+        websiteUrl: createTabUrl("silent-auth"),
+      });
+      saveEvent.notifySuccess();
     } else {
       microsoftTeams.settings.setSettings({
-        suggestedDisplayName: "Axyn Silent Auth (Access Token)",
+        suggestedDisplayName: "Axyn Button Auth (Access Token)",
         contentUrl: createTabUrl("access-token"),
         entityId: "AxynUbboUser",
         websiteUrl: createTabUrl("access-token"),
@@ -35,6 +43,8 @@ function createTabUrl(context) {
     return window.location.protocol + "//" + window.location.host + "/default-user-info";
   } else if (context == "access-token") {
     return window.location.protocol + "//" + window.location.host + "/tab-auth/simple";
+  } else if (context == "silent-auth") {
+    return window.location.protocol + "//" + window.location.host + "/tab-auth/silent";
   } else {
     return window.location.protocol + "//" + window.location.host + "/default";
   }
